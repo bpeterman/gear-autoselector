5039940bbe57298e7d05688d6f6e8cf4f10af4af
Blake Peterman <blake@peterman.io>
2022-02-27 22:48:41 -0700

    configure allowed hosts

--- gearselector/settings.py
+++ gearselector/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG") == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("HOST")]
 
 
 # Application definition
