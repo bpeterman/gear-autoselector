5065c670e55e4d8ff8ae961c02c2c0363d9b4879
Blake Peterman <blake@peterman.io>
2022-03-15 22:23:36 -0600

    Check permissions

--- exchange_token/views.py
+++ exchange_token/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,21 +7,22 @@ from selector.library.strava import save_refresh_token
 
 
 def exchange_token(request):
-    # TODO: Fix minimum scope
-    bare_minimum_scope = {"read", "activity:write", "activity:read_all"}
     code = request.GET.get("code")
     scope = request.GET.get("scope")
     scope_set = set(scope.split(","))
-    is_min_scope = True
-    for scope in bare_minimum_scope:
-        if scope not in scope_set:
-            is_min_scope = False
+    # TODO: check that if we request read_all we have an option to lower permissions
+    read_set = {"activity:read", "activity:read_all"}
+    if len(read_set.intersection(scope_set)) == 0:
+        return HttpResponse(
+            "We need to be able to read activities to determine future rides."
+        )
+    elif "activity:write" not in scope_set:
+        return HttpResponse(
+            "We need to be able to write activities to change your gear after the fact"
+        )
 
     save_refresh_token(code)
     # TODO: setup webhook
+    # TODO: Can you call django mgmt commands directly and detached?
 
-    if not is_min_scope:
-        return HttpResponse("You're missing the minimum amount of permissions")
-
-    # return HttpResponse("You're signed up!")
     return render(request, "exchange_token/index.html")
